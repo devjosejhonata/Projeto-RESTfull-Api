f746f6fbfc2c64b95f00bf926b9752920f1943ca
Jose Jhonata <contatojosejhonata@gmail.com>
2024-02-14 20:27:56 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+/*
+Validando dados recebidos via Post
+
+*/
+
+const express = require('express');
+//carregando o módulo http
+const consign = require('consign');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+let app = express();
+//invocando o express
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//todos os dados que eu receber via post, converte em json
+app.use(expressValidator());
+//invocando o expressValidator
+
+consign().include('routes').include('utils').into(app);
+//invocando o consign, incluindo a pasta routes, vai incluir todos os arquivos que eu criar em routes
+//incluindo em into(app)
+//lendo tambem a pasta utils, qualquer arquivo que colocar aqui, carrego tambem como módulo
+
+app.listen(3000, '127.0.0.1', () => {
+    console.log('Servidor rodando!');
+});
+//ouvindo o servidor na porta 3000
